de76beb6e9ea557b15867c3f3e77c5f3a69b9983
jonathanmusa <jhonathansamu91@gmail.com>
2020-12-18 21:37:46 -0500

    creating project

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <h1>My To-Do-List</h1>
+    <ul>
+      <li v-for="item in TodoItems" :key="item.id">
+        <TodoItem :label="item.label" :done="item.done" :id="item.id" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+import TodoItem from "@/components/ToDoItem.vue";
+import uniqueId from "lodash.uniqueid";
+
+export default {
+  name: "app",
+  components: {
+    TodoItem,
+  },
+  data() {
+    return {
+      TodoItems: [
+        { id: uniqueId("todo-"), label: "Learn Vue", done: false },
+        {
+          id: uniqueId("todo-"),
+          label: "Create a Vue project with the CLI",
+          done: false,
+        },
+        { id: uniqueId("todo-"), label: "Have fun", done: false },
+        { id: uniqueId("todo-"), label: "Create a to-do list", done: false },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+li{
+  list-style-type: none;
+}
+</style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="checkbox" id="todo-item" v-bind:checked="isDone" />
+    <!-- <input type="checkbox" id="todo-item" :checked="isDone" /> -->
+    <!-- <label for="todo-item">{{ label }}</label> -->
+    <label :for="id">{{ label }}</label>
+  </div>
+</template>
+
+<script>
+import uniqueid from "lodash.uniqueid";
+export default {
+  props: {
+    label: { required: true, type: String },
+    done: { default: false, type: Boolean },
+    id: { required: true, type: String },
+  },
+  data() {
+    return {
+      isDone: this.done,
+      /* id: uniqueid("todo-"), */
+    };
+  },
+};
+</script>
